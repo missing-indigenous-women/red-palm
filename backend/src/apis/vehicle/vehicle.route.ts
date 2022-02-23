cfa5cd2fdcb75d8c2c1d6484dae14d66cbe90dc4
Cynthia Nikolai <cynthianikolai@gmail.com>
2022-02-23 07:59:32 -0500

    vehicle CRUD operations methods

--- backend/src/apis/vehicle/vehicle.route.ts
+++ backend/src/apis/vehicle/vehicle.route.ts
@@ -3,13 +3,20 @@ import {getVehicleByVehicleId} from './vehicle.controller'
 import {postVehicle} from './vehicle.controller'
 import {updateVehicleByVehicleId} from './vehicle.controller'
 import {deleteVehicleByVehicleId} from './vehicle.controller'
+import {asyncValidatorController} from "../../utils/controllers/asyncValidator.controller";
+import {check, checkSchema} from "express-validator";
+import {vehicleValidator} from "./vehicle.validator";
 
 export const vehicleRoute = Router()
 
 vehicleRoute.route("/")
-    .post(postVehicle)
+    .post(asyncValidatorController(checkSchema(vehicleValidator)),postVehicle)
 
 vehicleRoute.route("/:vehicleId")
-    .get(getVehicleByVehicleId)
-    .put(updateVehicleByVehicleId)
-    .delete(deleteVehicleByVehicleId)
+    .get(asyncValidatorController([
+        check("vehicleId","please provide a valid vehicleId").isUUID()
+    ]),getVehicleByVehicleId)
+    .put(asyncValidatorController(checkSchema(vehicleValidator)),updateVehicleByVehicleId)
+    .delete(asyncValidatorController([
+        check("vehicleId","please provide a valid vehicleId").isUUID()
+    ]),deleteVehicleByVehicleId)
---
+++ backend/src/apis/vehicle/vehicle.validator.ts
@@ -0,0 +1,57 @@
+import {Schema} from 'express-validator';
+
+export const vehicleValidator : Schema = {
+    vehicleWomanId: {
+        isUUID: {
+            errorMessage: 'please provide a valid vehicleId'
+        }
+    },
+    vehicleColor: {
+        isLength: {
+            errorMessage: 'vehicle color cannot be longer than 60 characters',
+            options: {max: 60}
+        },
+        trim: true,
+        escape: true
+    },
+    vehicleDescription: {
+        isLength: {
+            errorMessage: 'vehicle description cannot be longer than 60 characters',
+            options: {max: 60}
+        },
+        trim: true,
+        escape: true
+    },
+    vehicleMake: {
+        isLength: {
+            errorMessage: 'vehicle make cannot be longer than 60 characters',
+            options: {max: 60}
+        },
+        trim: true,
+        escape: true
+    },
+    vehicleModel: {
+        isLength: {
+            errorMessage: 'vehicle model cannot be longer than 60 characters',
+            options: {max: 60}
+        },
+        trim: true,
+        escape: true
+    },
+    vehiclePlateNumber: {
+        isLength: {
+            errorMessage: 'vehicle license plate number cannot be longer than 60 characters',
+            options: {max: 60}
+        },
+        trim: true,
+        escape: true
+    },
+    vehicleYear: {
+        isLength: {
+            errorMessage: 'vehicle year cannot be longer than 60 characters',
+            options: {max: 60}
+        },
+        trim: true,
+        escape: true
+    }
+};
