b0c422860b739b690be7110d386583fbf2b278db
carlos perez <96350252+CarlosPerez505@users.noreply.github.com>
2022-02-23 15:02:40 -0700

    worked on appuser utils

--- backend/src/utils/appUser/selectAppUserByAppUserEmail.ts
+++ backend/src/utils/appUser/selectAppUserByAppUserEmail.ts
@@ -1,13 +1,13 @@
 import {connect} from "../database.utils";
-import {Profile} from "../interfaces/Profile";
+import {AppUser} from "../interfaces/AppUser";
 import {RowDataPacket} from 'mysql2';
 
-export async function selectAppUserByAppUserEmail(profileEmail: string): Promise<Profile|null>  {
+export async function selectAppUserByAppUserEmail(appUserEmail: string): Promise<AppUser|null>  {
 	try {
 		const mysqlConnection = await connect();
-		const sqlQuery: string = "SELECT BIN_TO_UUID(profileId) as profileId, profileActivationToken, profileAtHandle, profileAvatarUrl, profileEmail, profileHash, profilePhone FROM profile WHERE profileEmail = :profileEmail"
-		const result = await mysqlConnection.execute(sqlQuery, {profileEmail}) as RowDataPacket[]
-		const rows: Profile[]  = result[0] as Profile[]
+		const sqlQuery: string = "SELECT BIN_TO_UUID(appUserId) as appUserId, appUserActivationToken, appUserDisplayName, appUserEmail, appUserFirstName, appUserHash, appUserLastName FROM appUser WHERE appUserEmail = :appUserEmail"
+		const result = await mysqlConnection.execute(sqlQuery, {appUserEmail}) as RowDataPacket[]
+		const rows: AppUser[]  = result[0] as AppUser[]
 		return rows.length === 1 ? {...rows[0]} : null;
 	} catch (error) {
 		throw error
