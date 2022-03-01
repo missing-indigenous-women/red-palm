4e100e33f30b9fca1af0c2f12ae703c5257be742
Cynthia Nikolai <cynthianikolai@gmail.com>
2022-03-01 16:11:23 -0500

    created started titlebar and footer

---
+++ frontend/src/ui/shared/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Footer() {
+    return (
+        <footer className="container-fluid bg-dark p-3 text-center">
+            <p className="m-0 text-light">footer</p>
+        </footer>
+    )
+}
---
+++ frontend/src/ui/shared/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Container, Nav, Navbar } from "react-bootstrap";
+
+export function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg" className="ps-3">
+            <Container fluid>
+                <Navbar.Brand href="#home">Title</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#link">Link1</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
