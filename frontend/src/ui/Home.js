fd77ab3bea6b471b766e78b430dc051cd16d6f98
carlos perez <96350252+CarlosPerez505@users.noreply.github.com>
2022-03-07 14:20:13 -0700

    testing git commit

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {ReactComponent as Redpalm} from '../images/bloodyHandPrint.svg';
 import {ReactComponent as Mel} from '../images/MelanieMarieJames.jpg';
-import {Container, Card, Row, Button, Col, Image,} from "react-bootstrap";
+import {Container, Card, Row, Button, Col, Image, Carousel, } from "react-bootstrap";
 
 import style from "./Home.module.css"
 
@@ -11,7 +11,7 @@ export const Home = () => {
         <>
             <Container fluid className={style.Background}>
             <Container>
-                <Image src="native.jpg" className={style.MagnifyingGLass}/>
+                <img src={require("../images/magnifyingGlass.png")}/>
             </Container>
             <Container fluid>
                 <Row>
@@ -60,6 +60,42 @@ export const Home = () => {
                     </Col>
 
                 </Row>
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100 vw-100"
+                            src="../images/MelanieMarieJames.jpg/351x468?text=First slide&bg=373940"
+                            alt="First slide"
+                            style={{ width: '18rem'  }}
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x468?text=Second slide&bg=282c34"
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </Container>
             </Container>
         </>
