ecceb579e9df37ce78da9e404d6ddc7e2fac245a
carlos perez <96350252+CarlosPerez505@users.noreply.github.com>
2022-03-06 18:09:29 -0700

    making cards and background image responsive

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,60 +1,58 @@
 import React from "react"
 import {ReactComponent as Redpalm} from '../images/bloodyHandPrint.svg';
-import {Container, Card, Row, Button, Col} from "react-bootstrap";
-import nativeSouthWest from '../images/native.jpg'
+import {Container, Card, Row, Button, Col, Image,} from "react-bootstrap";
+
 import style from "./Home.module.css"
 
 
 export const Home = () => {
     return (
         <>
+            <Container>
+                <Image src="native.jpg" className={style.MagnifyingGLass}/>
+            </Container>
             <Container fluid>
                 <Row>
                     <Col className={"p-0"}><Redpalm style={{background: "black"}}/></Col>
-                    <Col  className={"text-center"} style={{background: "black"}}><h1 className={"text-white"}
-                                                                                              style={{background: "black"}}>Welcome
-                        To Redpalm</h1></Col>
+                    <Col  className={"text-center"} style={{background: "black"}}><h1 className={"text-white"} style={{background: "black"}}>Welcome To Redpalm</h1></Col>
                 </Row>
             </Container>
-            <Container fluid className={style.Background}>
-                <Row className="justify-content-md-between">
+            <Container fluid className={style.Background} >
+                <Row className="justify-content-md-between col-lg">
 
-                    <Col md={"auto"} >
-                        <Card className={style.Card1}  style={{width: '18rem'}}>
-                            <Redpalm/>
+                    <Col lg={4}>
+                        <Card   style={{width: '18rem'}}>
+                            <Redpalm style={{background: "black"}}/>
                             <Card.Body style={{background: "red"}}>
                                 <Card.Title>Card Title</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
+                                    Map with missing person's last known location
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Map</Button>
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col md={"auto"}>
-                        <Card className={style.Card2} style={{width: '18rem'}}>
-                            <Redpalm/>
+                    <Col lg={4}>
+                        <Card  style={{width: '18rem'}}>
+                            <Redpalm style={{background: "black"}}/>
                             <Card.Body style={{background: "red"}}>
                                 <Card.Title>Card Title</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
+                                    Missing person's case info
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Info</Button>
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col md={"auto"}>
-                        <Card className={style.Card3} style={{width: '18rem'}}>
-                            <Redpalm/>
+                    <Col lg={4}>
+                        <Card  style={{width: '18rem'}}>
+                            <Redpalm style={{background: "black"}}/>
                             <Card.Body style={{background: "red"}}>
                                 <Card.Title>Card Title</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
+                                    New information that you or anyone looking into the case might have
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Post info</Button>
                             </Card.Body>
                         </Card>
                     </Col>
