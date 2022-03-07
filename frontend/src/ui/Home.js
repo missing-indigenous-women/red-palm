62d4c1fa22a828b17a791cc878ffa6823dc3aa0c
carlos perez <carlosperez505@gmail.com>
2022-03-07 14:24:15 -0700

    testing github commits

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -88,7 +88,7 @@ export const Home = () => {
                         <img
                             className="d-block w-100"
                             src="holder.js/800x400?text=Third slide&bg=20232a"
-                            alt="Third slide"
+                            alt="Third slide of missing person"
                         />
                         <Carousel.Caption>
                             <h3>Third slide label</h3>
