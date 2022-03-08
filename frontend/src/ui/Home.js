641505f87bea7b02f5d78fe4c86db7719035277d
carlos perez <carlosperez505@gmail.com>
2022-03-08 15:22:14 -0700

    working on carousel

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {ReactComponent as Redpalm} from '../images/bloodyHandPrint.svg';
-import {ReactComponent as Mel} from '../images/MelanieMarieJames.jpg';
+import Mel from '../images/MelanieMarieJames.jpg';
 import {Container, Card, Row, Button, Col, Image, Carousel, } from "react-bootstrap";
 
 import style from "./Home.module.css"
@@ -60,44 +60,169 @@ export const Home = () => {
                     </Col>
 
                 </Row>
-                <Carousel>
-                    <Carousel.Item interval={1000}>
-                        <img
-                            className="d-block w-100 vw-100"
-                            src="../images/MelanieMarieJames.jpg/351x468?text=First slide&bg=373940"
-                            alt="First slide"
-                            style={{ width: '18rem'  }}
-                        />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={500}>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x468?text=Second slide&bg=282c34"
-                            alt="Second slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Third slide&bg=20232a"
-                            alt="Third slide of missing person"
-                        />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        <Carousel style={{width: '18rem'}}>
+                            <Carousel.Item interval={5000} >
+
+                                <img
+                                    className="d-block w-100 "
+                                    src={Mel}
+                                    alt="First slide"
+
+                                />
+                                <Carousel.Caption>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item interval={5000}>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77999/Thumbnail"
+                                    alt="Second slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Second slide label</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77997/Original"
+                                    alt="Third slide of missing person"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Third slide label</h3>
+                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <Carousel style={{width: '18rem'}}>
+                            <Carousel.Item interval={5000} >
+
+                                <img
+                                    className="d-block w-100 "
+                                    src={Mel}
+                                    alt="First slide"
+
+                                />
+                                <Carousel.Caption>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item interval={5000}>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77999/Thumbnail"
+                                    alt="Second slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Second slide label</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77997/Original"
+                                    alt="Third slide of missing person"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Third slide label</h3>
+                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        <Carousel style={{width: '18rem'}}>
+                            <Carousel.Item interval={5000} >
+
+                                <img
+                                    className="d-block w-100 "
+                                    src={Mel}
+                                    alt="First slide"
+
+                                />
+                                <Carousel.Caption>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item interval={5000}>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77999/Thumbnail"
+                                    alt="Second slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Second slide label</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77997/Original"
+                                    alt="Third slide of missing person"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Third slide label</h3>
+                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        <Carousel style={{width: '18rem'}}>
+                            <Carousel.Item interval={5000} >
+
+                                <img
+                                    className="d-block w-100 "
+                                    src={Mel}
+                                    alt="First slide"
+
+                                />
+                                <Carousel.Caption>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item interval={5000}>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77999/Thumbnail"
+                                    alt="Second slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Second slide label</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://www.namus.gov/api/CaseSets/NamUs/MissingPersons/Cases/39942/Images/77997/Original"
+                                    alt="Third slide of missing person"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Third slide label</h3>
+                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </Col>
+                </Row>
             </Container>
             </Container>
+
         </>
     )
 }
