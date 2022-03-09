0b46cc170a0947401c560c5dcd48223d56112960
Cynthia Nikolai <cynthianikolai@gmail.com>
2022-03-09 16:43:56 -0500

    fixing merge conflict

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -6,11 +6,15 @@ import {FourOhFour} from './FourOhFour'
 import React from 'react'
 import {NavBar} from "./shared/NavBar";
 import {Footer} from "./shared/Footer";
+<<<<<<< HEAD
 import {InfoForm} from "./shared/InfoForm";
 import {Provider} from "react-redux";
 import {InfoPage} from "./InfoPage";
 import {Signup} from "./Signup";
 import {Signin} from "./Signin";
+=======
+import {Provider} from "react-redux";
+>>>>>>> 8eb6c78 (got simple store working for map page)
 
 export const App = (store) => (
     <>
@@ -20,10 +24,13 @@ export const App = (store) => (
                 <Switch>
                     <Route exact path='/' component={Home}/>
                     <Route exact path='/map' component={Map}/>
+<<<<<<< HEAD
                     <Route exact path='/InfoPage' component={InfoPage}/>
                     <Route exact path='/InfoForm' component={InfoForm}/>
                     <Route exact path='/Signup' component={Signup}/>
                     <Route exact path='/Signin' component={Signin} />
+=======
+>>>>>>> 8eb6c78 (got simple store working for map page)
                     <Route component={FourOhFour}/>
                 </Switch>
                 <Footer/>
--- frontend/src/ui/Map.js
+++ frontend/src/ui/Map.js
@@ -24,7 +24,11 @@ export const Map = () => {
      * E.g when a network request to an api has completed and there is new data to display on the dom.
      */
     useEffect(sideEffects, [dispatch])
+<<<<<<< HEAD
     //console.log(women)
+=======
+    console.log(women)
+>>>>>>> 8eb6c78 (got simple store working for map page)
     return (
         <>
             <Container className={'py-4'}>
@@ -37,7 +41,11 @@ export const Map = () => {
                 </Form>
             </Container>
             <Container className="text-center pb-5">
+<<<<<<< HEAD
                <Image fluid src={"https://via.placeholder.com/1000x800"} alt={"map of missing women"}/>
+=======
+    `   `        <Image fluid src={"https://via.placeholder.com/1000x800"} alt={"map of missing women"}/>
+>>>>>>> 8eb6c78 (got simple store working for map page)
             </Container>
             <Container className={'pb-5'}>
                 {/*{women.map(woman => <MissingWoman woman={woman} />)}*/}
