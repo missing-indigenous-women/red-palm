ce9115bf85c0025b58c8afe82a098d0c8f6c5790
Cynthia Nikolai <cynthianikolai@gmail.com>
2022-03-12 17:14:24 -0500

    cleaning up data

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,6 +5,5 @@ import women from "./women"
 import {configureStore} from '@reduxjs/toolkit'
 
 const reducer = combineReducers({vehicles, women})
-//const reducer = combineReducers({users, vehicles})
 //In order to use redux a store must be initialized and passed to the Provider component.
 export const store = configureStore({reducer})
--- frontend/src/ui/DetailPage.js
+++ frontend/src/ui/DetailPage.js
@@ -32,6 +32,11 @@ export const DetailPage = ({match}) => {
             : null
     ));
 
+    const datefunction = (dateparameter) => {
+        let myDate = new Date(dateparameter);
+        return (myDate.getMonth() + 1) + '/' + myDate.getDate() + '/' + myDate.getFullYear();
+    }
+
     return (
         <>
             <Container fluid className={style.Background}>
@@ -45,8 +50,8 @@ export const DetailPage = ({match}) => {
                                     <Card.Title></Card.Title>
                                     <div className={style.Info}>
                                         <p>Aliases: {woman.womanAliases}</p>
-                                        <p>DateOfDisappearance: {woman.womanDateOfDisappearance}</p>
-                                        <p>DateOfBirth: {woman.womanDateOfBirth}</p>
+                                        <p>DateOfDisappearance: {datefunction(woman.womanDateOfDisappearance)}</p>
+                                        <p>DateOfBirth: {datefunction(woman.womanDateOfBirth)}</p>
                                         <p>EyeColor:m {woman.womanEyeColor}</p>
                                         <p>FavoriteHangoutPlaces: {woman.womanFavoriteHangoutPlaces}</p>
                                         <p>FirstName: {woman.womanFirstName}</p>
