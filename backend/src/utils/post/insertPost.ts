181217a045e863fcc6c240c85168d923a8f190db
carlos perez <carlosperez505@gmail.com>
2022-03-13 00:10:56 -0700

    Added cloudinary url for avatar

--- backend/src/utils/post/insertPost.ts
+++ backend/src/utils/post/insertPost.ts
@@ -5,7 +5,7 @@ import {ResultSetHeader, RowDataPacket} from 'mysql2';
 export async function insertPost(post: Post) : Promise<string> {
     try {
         const mySqlConnection = await connect()
-        const mySqlQuery = "INSERT INTO post(postId, postAppUserId, postWomanId, postDate, postText) VALUES(UUID_TO_BIN(UUID()), UUID_TO_BIN(:postAppUserId),UUID_TO_BIN(:postWomanId), NOW(), :postText)";
+        const mySqlQuery = "INSERT INTO post(postId, postAppUserId, postWomanId, postDate, postText) VALUES(UUID_TO_BIN(UUID()), UUID_TO_BIN(:postAppUserId, ),UUID_TO_BIN(:postWomanId), NOW(), :postText)";
 
         const [result]= await mySqlConnection.execute(mySqlQuery, post) as [ResultSetHeader, RowDataPacket]
         return "Post created successfully"
