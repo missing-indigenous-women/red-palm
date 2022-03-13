5d09d244fb3a65be9e271dbe53ebdc4cc4241268
carlos perez <carlosperez505@gmail.com>
2022-03-12 23:59:46 -0700

    deleted some images

--- backend/src/apis/image-upload/image-upload.route.ts
+++ backend/src/apis/image-upload/image-upload.route.ts
@@ -5,7 +5,7 @@ export async function imageUploadController(request: Request, response: Response
     try {
         // uncomment if in production
         const message : string = await uploadToCloudinary(request)
-        //const message : string =  "http://placekitten.com/150/150"
+        const message : string =  "https://res.cloudinary.com/redpalm/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1647114966/redPalm%20avatars/eagle_tjznbz.svg"
         return response.json({status: 200, data: null, message: message})
     } catch (error: any) {
         return response.json({status:400, message: error.message, data: null})
