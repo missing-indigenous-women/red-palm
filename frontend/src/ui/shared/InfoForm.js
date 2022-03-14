eda8aa2fe79f53cdc10674cffa4ef74c753a6fff
MariaElena Barboa-Reyes <Mbarboareyes@cnm.edu>
2022-03-13 20:44:42 -0600

    have some problems

--- frontend/src/ui/shared/InfoForm.js
+++ frontend/src/ui/shared/InfoForm.js
@@ -104,161 +104,180 @@ export const InfoForm = (props) => {
 
         } = props;
         return (<>
-            <Container className="mb-3">
-                <h1>Information Form</h1>
-                <Image src={flower} alt={flower} height="200px"/>
-            </Container>
-            <form onSubmit={handleSubmit}>
-                <Container>
-                    <Row>
-                        <Col md={6}>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>First Name:</InputGroup.Text>
-                                <FormControl aria-label="First Name"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanFirstName}
-                                             name={'womanFirstName'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Last Name:</InputGroup.Text>
-                                <FormControl aria-label="Last name Name"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanLastName}
-                                             name={'womanLastName'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Nick Name or other Names:</InputGroup.Text>
-                                <FormControl aria-label="Nick-Name Or other Names"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanAliases}
-                                             name={'womanAliases'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Missing Since:</InputGroup.Text>
-                                <FormControl aria-label="Missing-Since"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanDateOfDisappearance}
-                                             name={'womanDateOfDisappearance'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Date of Birth</InputGroup.Text>
-                                <FormControl aria-label="Date-Of-Birth"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanDateOfBirth}
-                                             name={'womanDateOfBirth'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Last seen address</InputGroup.Text>
-                                <FormControl aria-label="age"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanLastLocation}
-                                             name={'womanLastLocation'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Tribe</InputGroup.Text>
-                                <FormControl aria-label="Tribe"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanTribe}
-                                             name={'womanTribe'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Latitude:</InputGroup.Text>
-                                <FormControl aria-label="Latitude"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanLatitude}
-                                             name={'womanLatitude'}
-                                />
-                            </InputGroup>
-                        </Col>
-                        <Col md={6}>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Eye Color:</InputGroup.Text>
-                                <FormControl aria-label="eye color"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanEyeColor}
-                                             name={'womanEyeColor'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Weight:</InputGroup.Text>
-                                <FormControl aria-label="body type"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanWeight}
-                                             name={'womanWeight'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Tattoo, and other Identifying marks:</InputGroup.Text>
-                                <FormControl aria-label="Tattoo"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanIdentifyingMarks}
-                                             name={'womanIdentifyingMarks'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Hobbies and Interest</InputGroup.Text>
-                                <FormControl aria-label="clothes"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanHobbiesAndInterests}
-                                             name={'womanHobbiesAndInterests'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Favorite hangout places</InputGroup.Text>
-                                <FormControl aria-label="body type"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanFavoriteHangoutPlaces}
-                                             name={'womanFavoriteHangoutPlaces'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Height:</InputGroup.Text>
-                                <FormControl aria-label="Tattoo"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanHeight}
-                                             name={'womanHeight'}
-                                />
-                            </InputGroup>
-                            <InputGroup className="mb-3">
-                                <InputGroup.Text>Hair Color:</InputGroup.Text>
-                                <FormControl aria-label="hair color"
-                                             onChange={handleChange}
-                                             onBlur={handleBlur}
-                                             value={values.womanHairColor}
-                                             name={'womanHairColor'}
-                                />
-                            </InputGroup>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col mb={2}>
-                            <Button variant="primary">Submit</Button>
-                            <Button variant="danger">Cancel</Button>
-                        </Col>
-                    </Row>
-                </Container>
-            </form>
-        </>)
+        <Container className="mb-3">
+            <h1>Information Form</h1>
+            <Image src={flower} alt={flower} height="200px"/>
+        </Container>
+        <form onSubmit={handleSubmit}>
+            <Container>
+                <Row>
+                    <Col md={6}>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>First Name:</InputGroup.Text>
+                            <FormControl aria-label="First Name"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanFirstName}
+                                         name={'womanFirstName'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Last Name:</InputGroup.Text>
+                            <FormControl aria-label="Last name Name"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanLastName}
+                                         name={'womanLastName'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Nick Name or other Names:</InputGroup.Text>
+                            <FormControl aria-label="Nick-Name Or other Names"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanAliases}
+                                         name={'womanAliases'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Missing Since:</InputGroup.Text>
+                            <FormControl aria-label="Missing-Since"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanDateOfDisappearance}
+                                         name={'womanDateOfDisappearance'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Date of Birth</InputGroup.Text>
+                            <FormControl aria-label="Date-Of-Birth"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanDateOfBirth}
+                                         name={'womanDateOfBirth'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Last seen address</InputGroup.Text>
+                            <FormControl aria-label="age"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanLastLocation}
+                                         name={'womanLastLocation'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Tribe</InputGroup.Text>
+                            <FormControl aria-label="Tribe"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanTribe}
+                                         name={'womanTribe'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Latitude:</InputGroup.Text>
+                            <FormControl aria-label="Latitude"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanLatitude}
+                                         name={'womanLatitude'}
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col md={6}>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Eye Color:</InputGroup.Text>
+                            <FormControl aria-label="eye color"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanEyeColor}
+                                         name={'womanEyeColor'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Weight:</InputGroup.Text>
+                            <FormControl aria-label="body type"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanWeight}
+                                         name={'womanWeight'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Tattoo, and other Identifying marks:</InputGroup.Text>
+                            <FormControl aria-label="Tattoo"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanIdentifyingMarks}
+                                         name={'womanIdentifyingMarks'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Hobbies and Interest</InputGroup.Text>
+                            <FormControl aria-label="clothes"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanHobbiesAndInterests}
+                                         name={'womanHobbiesAndInterests'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Favorite hangout places</InputGroup.Text>
+                            <FormControl aria-label="body type"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanFavoriteHangoutPlaces}
+                                         name={'womanFavoriteHangoutPlaces'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Height:</InputGroup.Text>
+                            <FormControl aria-label="Tattoo"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanHeight}
+                                         name={'womanHeight'}
+                            />
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>Hair Color:</InputGroup.Text>
+                            <FormControl aria-label="hair color"
+                                         onChange={handleChange}
+                                         onBlur={handleBlur}
+                                         value={values.womanHairColor}
+                                         name={'womanHairColor'}
+                            />
+                        </InputGroup>
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>Longitude:</InputGroup.Text>
+                        <FormControl aria-label="Longitude"
+                                     onChange={handleChange}
+                                     onBlur={handleBlur}
+                                     value={values.womanLastLocation}
+                                     name={'womanLastLocation'}
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>Photo:</InputGroup.Text>
+                        <FormControl aria-label="Longitude"
+                                     onChange={handleChange}
+                                     onBlur={handleBlur}
+                                     value={values.womanPhoto1}
+                                     name={'womanPhoto1'}
+                        />
+                    </InputGroup>
+                </Col>
+            </Row>
+            <Row>
+                <Col mb={2}>
+                    <Button variant="primary">Submit</Button>
+                    <Button variant="danger">Cancel</Button>
+                </Col>
+            </Row>
+        </Container>
+        </form>
+    </>)
     }
 }
