95d7cdff4fc597a32811eb7594446870e6ae9dc5
dmars <dmars@cnm.edu>
2022-03-14 09:50:08 -0600

    saving private ryan

--- frontend/src/ui/shared/NavBar.js
+++ frontend/src/ui/shared/NavBar.js
@@ -7,16 +7,23 @@ import {Route} from "react-router";
 
 export function NavBar() {
     return (
-        <Navbar bg="dark" expand="lg" className="ps-3">
+
+        <Navbar expand="lg" className="ps-3 sticky-top">
             <Container fluid>
-                <Navbar.Brand href="#home">Title</Navbar.Brand>
+                <Navbar.Brand href="/"><Col className='mt-3'><Redpalm  style={{height:70, width:70, background: "black"}}/> </Col></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#link">Link1</Nav.Link>
-                        <Nav.Link href="#link">Link1</Nav.Link>
-                        <Nav.Link href="#link">Link1</Nav.Link>
-                        <Nav.Link href="#link">Link1</Nav.Link>
+                        <Route render={({history}) => (
+                            <>
+                        <Nav.Link onClick={() => {
+                            history.push(`/`)}}>Home</Nav.Link>
+                        <Nav.Link onClick={() => {
+                            history.push(`/sign-in`)}}>Sign-in</Nav.Link>
+                            <Nav.Link onClick={() => {
+                            history.push(`/map`)}}> Map</Nav.Link>
+                            </>
+                            )}/>
 
                     </Nav>
                 </Navbar.Collapse>
