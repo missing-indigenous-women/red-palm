cc8d136161711c38f1a03426bb3acaadf92ccb6a
carlos perez <carlosperez505@gmail.com>
2022-03-15 00:03:06 -0600

    Git merge

--- frontend/src/ui/FourOhFour.js
+++ frontend/src/ui/FourOhFour.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Col, Container, Row} from "react-bootstrap";
 
-export const FourOhFour = () => {
+export const FourOhFour = () => {git
     return (
         <>
             <h1>File not found.</h1>
