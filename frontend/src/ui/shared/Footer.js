6d1e04e51b91651b9179383cf32a77bfa299c36d
carlos perez <carlosperez505@gmail.com>
2022-04-13 01:17:35 -0600

    changing order

--- frontend/src/ui/shared/Footer.js
+++ frontend/src/ui/shared/Footer.js
@@ -7,20 +7,21 @@ export function Footer() {
         <footer className="container-fluid bg-dark p-3 text-center">
             <Row>
                 <Col>
-                    <a href={'https://www.cindynikolai.us/'}>Cindy</a><br/>
+                    <a href={'https://www.cp3develops.com/'}>Carlos</a><br/>
+
                 </Col>
                 <Col>
-                    <a href={'http://emmanuelpricemars.com/'}>Emmanuel</a>
+                    <a href={'https://www.cindynikolai.us/'}>Cindy </a>
                 </Col>
                 <Col>
                     <p className="m-0 text-light">created by</p>
                     <p className="m-0 text-light">2022</p>
                 </Col>
                 <Col>
-                    <a href={'http://mariaebarboareyes.com/'}>Maria</a>
+                    <a href={'https://mariaebarboareyes.com/'}>Maria</a>
                 </Col>
                 <Col>
-                    <a href={'http://www.cp3develops.com/'}>Carlos</a>
+                    <a href={'https://emmanuelpricemars.com/'}>Emmanuel </a>
                 </Col>
             </Row>
         </footer>
